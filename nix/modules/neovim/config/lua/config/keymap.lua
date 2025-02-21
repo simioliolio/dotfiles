67fed65b66101d54da0b0263d33b65c307bc4d63
Simon Haycock <shaycock@spotify.com>
2025-02-21 17:21:02 +0000

    Add nvim-lint and keymap for fzf grep

--- nix/modules/neovim/config/lua/config/keymap.lua
+++ nix/modules/neovim/config/lua/config/keymap.lua
@@ -1,2 +1,6 @@
 vim.keymap.set('n', '<Leader>ff', function() require("fzf-lua").files() end, { noremap = true, silent = true, desc = "FzfLua files"})
 
+vim.keymap.set('n', '<Leader>gg', function() require("fzf-lua").grep_project() end, { noremap = true, silent = true, desc = "FzfLua grep_project"})
+
+vim.keymap.set("n", "<Leader>ll", function() require("lint").try_lint() end, { desc = "Lint current file" })
+
--- nix/modules/neovim/default.nix
+++ nix/modules/neovim/default.nix
@@ -1,7 +1,14 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
-  # TODO: Needed?
-  programs.neovim.defaultEditor = true;
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    extraPackages = with pkgs; [
+      ruff
+      ruff-lsp
+      stylua
+    ];
+  };
 
   home.file.".config/nvim" = {
     source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/dotfiles/nix/modules/neovim/config";
