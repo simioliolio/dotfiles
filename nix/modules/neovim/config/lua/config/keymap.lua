7ca747a63a7e6a03da3cfa3b6a1d45f646286dbb
Simon Haycock <shaycock@spotify.com>
2025-03-03 16:01:02 +0000

    Add keymap for FzfLua buffers. Update Lazy

--- nix/modules/neovim/config/lua/config/keymap.lua
+++ nix/modules/neovim/config/lua/config/keymap.lua
@@ -2,5 +2,7 @@ vim.keymap.set('n', '<Leader>ff', function() require("fzf-lua").files() end, { n
 
 vim.keymap.set('n', '<Leader>gg', function() require("fzf-lua").grep_project() end, { noremap = true, silent = true, desc = "FzfLua grep_project"})
 
+vim.keymap.set('n', '<Leader>bb', function() require("fzf-lua").buffers() end, { noremap = true, silent = true, desc = "FzfLua buffers"})
+
 vim.keymap.set("n", "<Leader>ll", function() require("lint").try_lint() end, { desc = "Lint current file" })
 
