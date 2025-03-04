1bc164e10edd9df4da30edb00cd5f626c1d492f5
Simon Haycock <shaycock@spotify.com>
2025-03-04 10:22:01 +0000

    Add custom function to coy relative path to clipboard

--- nix/modules/neovim/config/lua/config/keymap.lua
+++ nix/modules/neovim/config/lua/config/keymap.lua
@@ -6,3 +6,8 @@ vim.keymap.set('n', '<Leader>bb', function() require("fzf-lua").buffers() end, {
 
 vim.keymap.set("n", "<Leader>ll", function() require("lint").try_lint() end, { desc = "Lint current file" })
 
+vim.api.nvim_create_user_command("Cppath", function()
+    local path = vim.fn.expand("%")
+    vim.fn.setreg("+", path)
+    vim.notify('Copied "' .. path .. '" to the clipboard!')
+end, {})
