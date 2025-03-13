3250ca7d7898fd826f4287e5f72bda57fd5492f4
Simon Haycock <shaycock@spotify.com>
2025-03-13 16:11:30 +0000

    Use live_grep instead of grep_project in Fzflua

--- nix/modules/neovim/config/lua/config/keymap.lua
+++ nix/modules/neovim/config/lua/config/keymap.lua
@@ -1,6 +1,6 @@
 vim.keymap.set('n', '<Leader>ff', function() require("fzf-lua").files() end, { noremap = true, silent = true, desc = "FzfLua files"})
 
-vim.keymap.set('n', '<Leader>gg', function() require("fzf-lua").grep_project() end, { noremap = true, silent = true, desc = "FzfLua grep_project"})
+vim.keymap.set('n', '<Leader>gg', function() require("fzf-lua").live_grep() end, { noremap = true, silent = true, desc = "FzfLua live_grep"})
 
 vim.keymap.set('n', '<Leader>bb', function() require("fzf-lua").buffers() end, { noremap = true, silent = true, desc = "FzfLua buffers"})
 
