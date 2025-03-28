ced1e796742902d6f0dfa2bcb0bc894393f2af90
Simon Haycock <shaycock@spotify.com>
2025-03-28 11:41:28 +0000

    Move fzf neovim plugin config to fzf.lua and add keymap for showing jumps

--- nix/modules/neovim/config/lua/config/keymap.lua
+++ nix/modules/neovim/config/lua/config/keymap.lua
@@ -1,9 +1,3 @@
-vim.keymap.set('n', '<Leader>ff', function() require("fzf-lua").files() end, { noremap = true, silent = true, desc = "FzfLua files"})
-
-vim.keymap.set('n', '<Leader>gg', function() require("fzf-lua").live_grep() end, { noremap = true, silent = true, desc = "FzfLua live_grep"})
-
-vim.keymap.set('n', '<Leader>bb', function() require("fzf-lua").buffers() end, { noremap = true, silent = true, desc = "FzfLua buffers"})
-
 vim.keymap.set("n", "<Leader>ll", function() require("lint").try_lint() end, { desc = "Lint current file" })
 
 vim.api.nvim_create_user_command("Cppath", function()
--- nix/modules/neovim/config/lua/plugins/fzf.lua
+++ nix/modules/neovim/config/lua/plugins/fzf.lua
@@ -4,5 +4,11 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   -- or if using mini.icons/mini.nvim
   -- dependencies = { "echasnovski/mini.icons" },
-  opts = {}
+  opts = {},
+  config = function()
+    vim.keymap.set('n', '<Leader>ff', function() require("fzf-lua").files() end, { noremap = true, silent = true, desc = "FzfLua files"})
+    vim.keymap.set('n', '<Leader>gg', function() require("fzf-lua").live_grep() end, { noremap = true, silent = true, desc = "FzfLua live_grep"})
+    vim.keymap.set('n', '<Leader>bb', function() require("fzf-lua").buffers() end, { noremap = true, silent = true, desc = "FzfLua buffers"})
+    vim.keymap.set('n', '<Leader>jj', function() require("fzf-lua").buffers() end, { noremap = true, silent = true, desc = "FzfLua jumps"})
+  end,
 }
