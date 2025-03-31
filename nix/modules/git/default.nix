cb07d80ede852034c7a4ed747b1f4afca0016f0d
Simon Haycock <shaycock@spotify.com>
2025-03-31 22:11:32 +0100

    Add oneline log git alias

--- nix/modules/git/default.nix
+++ nix/modules/git/default.nix
@@ -4,6 +4,7 @@
     enable = true;
     aliases = {
       br = "for-each-ref --sort=committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'";
+      l = "log --pretty=format:'%C(cyan)%h%Creset %C(green)%ad%Creset %C(yellow)%an%Creset: %C(reset)%s' --date=short --abbrev-commit";
     };
     extraConfig = {
       core = {
