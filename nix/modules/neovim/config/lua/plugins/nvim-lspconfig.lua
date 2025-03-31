f92348e4c514c5b2704603a1b4e4af986ab66729
Simon Haycock <shaycock@spotify.com>
2025-03-31 19:22:13 +0100

    Add keymap for going to next and previous vim diagnostics for lsp

--- nix/modules/neovim/config/lua/plugins/nvim-lspconfig.lua
+++ nix/modules/neovim/config/lua/plugins/nvim-lspconfig.lua
@@ -24,6 +24,8 @@ return {
         vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
         vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
         vim.keymap.set('n', '<leader>fo', function() vim.lsp.buf.format { async = true } end, bufopts)
+        vim.keymap.set('n', '<leader>dn', vim.diagnotic.goto_next, bufopts)
+        vim.keymap.set('n', '<leader>dp', vim.diagnotic.goto_prev, bufopts)
       end,
     }
 
