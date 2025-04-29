230da6be65aba762a97639739898e25e0927c8a5
Simon Haycock <shaycock@spotify.com>
2025-04-29 19:07:56 +0100

    pyenv init

--- nix/modules/zsh/default.nix
+++ nix/modules/zsh/default.nix
@@ -17,6 +17,7 @@
       export NVM_DIR="${config.home.homeDirectory}/.nvm"
       [ -s "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" ] && \. "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" # This loads nvm
       [ -s "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm" ] && \. "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
+      eval "$(pyenv init -)"
     '';
 
     shellAliases = {
