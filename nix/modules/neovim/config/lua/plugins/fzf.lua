b188a71b739739536479787a71ff36f208abfe99
Simon Haycock <shaycock@spotify.com>
2025-06-13 15:03:38 +0100

    Improve fzf keymaps

--- nix/modules/neovim/config/lua/plugins/fzf.lua
+++ nix/modules/neovim/config/lua/plugins/fzf.lua
@@ -9,16 +9,29 @@ return {
     vim.keymap.set('n', '<Leader>ff', function() require("fzf-lua").files() end, { noremap = true, silent = true, desc = "FzfLua files"})
     vim.keymap.set('n', '<Leader>gg', function() require("fzf-lua").live_grep() end, { noremap = true, silent = true, desc = "FzfLua live_grep"})
     vim.keymap.set('n', '<Leader>bb', function() require("fzf-lua").buffers() end, { noremap = true, silent = true, desc = "FzfLua buffers"})
-    vim.keymap.set('n', '<Leader>jj', function() require("fzf-lua").buffers() end, { noremap = true, silent = true, desc = "FzfLua jumps"})
+    vim.keymap.set('n', '<Leader>jj', function() require("fzf-lua").jumps() end, { noremap = true, silent = true, desc = "FzfLua jumps"})
+    vim.keymap.set('n', '<Leader>kk', function() require("fzf-lua").keymaps() end, { noremap = true, silent = true, desc = "FzfLua keymaps"})
     vim.keymap.set('n', '<Leader>gp', function ()
       local current_file_dir = vim.fn.fnamemodify(vim.fn.expand('%:p'), ':h')
       if current_file_dir and current_file_dir ~= '' then
         require("fzf-lua").live_grep({
           cwd = current_file_dir,
+          prompt = 'Live grep in ' .. current_file_dir .. '>',
         })
       else
         vim.notify('Not in a file buffer.')
       end
     end, { desc = "Grep in current file's directory" })
+    vim.keymap.set('n', '<Leader>g/', function ()
+      local last_search_term = vim.fn.getreg('/')
+      if last_search_term == '' then
+        vim.notify("No current search term found", vim.log.levels.INFO, { title = "fzf-lua g/" })
+      end
+      require("fzf-lua").live_grep({
+        query = last_search_term,
+        case_mode = 'respect_case',
+      })
+    end)
+    require("fzf-lua").setup({ winopts = { preview = { wrap = true, layout = "horizonal" }}})
   end,
 }
