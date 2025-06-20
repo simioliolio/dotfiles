271d2f8d62e83a2bfbae30bfdf03043db7dc2540
simioliolio <simioliolio@bitbasic.co.uk>
2025-06-20 10:07:20 +0100

    Manually manage git config dotfile

--- nix/modules/git/default.nix
+++ nix/modules/git/default.nix
@@ -1,24 +1,11 @@
-{ ... }:
+{ config, ... }:
 {
   programs.git = {
     enable = true;
-    aliases = {
-      br = "for-each-ref --sort=committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'";
-      l = "log --pretty=format:'%C(cyan)%h%Creset %C(green)%ad%Creset %C(yellow)%an%Creset: %C(reset)%s' --date=short --abbrev-commit";
-    };
-    extraConfig = {
-      core = {
-        editor = "vim";
-      };
-      include.path = "~/.config/git/writable_config";
-      push = {
-        autoSetupRemote = true;
-      };
-      pull = {
-        rebase = true;
-      };
-    };
     lfs.enable = true;
+  };
+  home.file.".config/git/config" = {
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/src/dotfiles/nix/modules/git/config";
   };
 }
 
