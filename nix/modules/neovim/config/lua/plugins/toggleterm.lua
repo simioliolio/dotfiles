fc127417867605bc35c2e1e554560d5eee2fe5fb
simioliolio <simioliolio@bitbasic.co.uk>
2025-09-03 07:28:08 +0100

    Start toggleterm at 75% of the window

--- nix/modules/neovim/config/lua/plugins/toggleterm.lua
+++ nix/modules/neovim/config/lua/plugins/toggleterm.lua
@@ -2,7 +2,18 @@ return {
   'akinsho/toggleterm.nvim',
   config = function()
     local toggleterm = require('toggleterm')
-    toggleterm.setup{}
+    toggleterm.setup{
+      size = function(term)
+        local v_height_percentage = 0.75
+        if term.direction == "horizontal" then
+          -- vim.o.lines is the total height of the nvim window
+          return math.floor(vim.o.lines * v_height_percentage)
+        elseif term.direction == "vertical" then
+          -- vim.o.columns is the total width of the nvim window
+          return math.floor(vim.o.columns * 0.5)
+        end
+      end
+    }
     vim.keymap.set('n', '<C-`>', '<cmd>ToggleTerm<cr>' )
     vim.keymap.set('t', '<C-`>', '<cmd>ToggleTerm<cr>' )
 
