bb4524864b665ae6e1625cb0c8853a2e2f05075a
simioliolio <simioliolio@bitbasic.co.uk>
2025-09-09 15:36:23 +0100

    Rework lspconfig. Enable mypy

--- nix/modules/neovim/config/lua/plugins/nvim-lspconfig.lua
+++ nix/modules/neovim/config/lua/plugins/nvim-lspconfig.lua
@@ -1,40 +1,51 @@
 return {
   "neovim/nvim-lspconfig",
-
+  dependencies = { 'saghen/blink.cmp' },
   config = function()
     local vim = vim
+    local lspconfig = require("lspconfig")
+
     local jump_and_show_next = function()
       vim.diagnostic.jump({ count = 1, float = true })
     end
+
     local jump_and_show_previous = function()
       vim.diagnostic.jump({ count = -1, float = true })
     end
 
-    vim.api.nvim_create_autocmd('LspAttach', {
-      callback = function(ev)
-        local bufopts = { noremap = true, silent = true, buffer = ev.buf }
-        vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, bufopts)
-        vim.keymap.set('n', '<leader>K', vim.lsp.buf.hover, bufopts)
-        vim.keymap.set('n', '<leader>gi', vim.lsp.buf.implementation, bufopts)
-        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
-        vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
-        vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, bufopts)
-        vim.keymap.set('n', '<leader>fo', function() vim.lsp.buf.format { async = true } end, bufopts)
-        vim.keymap.set('n', ']d', jump_and_show_next, bufopts)
-        vim.keymap.set('n', '[d', jump_and_show_previous, bufopts)
+    local on_attach = function(client, bufnr)
+      -- NOTE: We wrap this in a pcall to prevent errors if a server doesn't
+      -- support formatting.
+      local status_ok, _ = pcall(client.supports_method, "textDocument/formatting")
+      if status_ok then
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          group = vim.api.nvim_create_augroup("LspFormat." .. bufnr, {}),
+          buffer = bufnr,
+          callback = function()
+            vim.lsp.buf.format({ bufnr = bufnr })
+          end,
+        })
       end
-    })
+      local bufopts = { noremap = true, silent = true, buffer = bufnr }
+      vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, bufopts)
+      vim.keymap.set('n', '<leader>K', vim.lsp.buf.hover, bufopts)
+      vim.keymap.set('n', '<leader>gi', vim.lsp.buf.implementation, bufopts)
+      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+      vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, bufopts)
+      vim.keymap.set('n', '<leader>fo', function() vim.lsp.buf.format { async = true } end, bufopts)
+      vim.keymap.set('n', ']d', jump_and_show_next, bufopts)
+      vim.keymap.set('n', '[d', jump_and_show_previous, bufopts)
+    end
 
-    vim.lsp.enable('lua_ls')
-    vim.lsp.config('lua_ls', {
-      on_init = function(client)
-        if client.workspace_folders then
-          local path = client.workspace_folders[1].name
-          if path ~= vim.fn.stdpath('config') and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')) then
-            return
-          end
-        end
-        client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+    local client_capabilities = vim.lsp.protocol.make_client_capabilities()
+    local capabilities = require('blink-cmp').get_lsp_capabilities(client_capabilities)
+
+    lspconfig.lua_ls.setup({
+      on_attach = on_attach,
+      capabilities = capabilities,
+      settings = {
+        Lua = {
           runtime = {
             -- Tell the language server which version of Lua you're using (most
             -- likely LuaJIT in the case of Neovim)
@@ -57,26 +68,32 @@ return {
               -- '${3rd}/busted/library'
             }
           }
-        })
-      end,
-      settings = {
-        Lua = {}
+        }
       }
     })
-    vim.lsp.enable('pylsp')
-    vim.lsp.config('pylsp', {
+
+    lspconfig.pylsp.setup({
+      on_attach = on_attach,
+      capabilities = capabilities,
       settings = {
         pylsp = {
           plugins = {
-            mypy = {
-              enabled = false
-            }
-          }
-        }
-      }
-
+            black = { enabled = true },
+            mypy = { enabled = true },
+            yapf = { enabled = false },
+          },
+        },
+      },
     })
-    vim.lsp.enable('ruff')
-    vim.lsp.enable("kotlin-language-server")
+
+    -- lspconfig.ruff.setup({
+    --   on_attach = on_attach,
+    --   capabilities = capabilities,
+    -- })
+
+    lspconfig['kotlin_language_server'].setup({
+      on_attach = on_attach,
+      capabilities = capabilities,
+    })
   end,
 }
